75a1636625524f8b438b4c0625f2358f9b0b0426
David Deville <david.deville59@gmail.com>
2022-11-16 13:00:18 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+1.times do
+  Restaurant.create!(name: "Takatoukité", address: "2 avenue de je sais pas", phone_number: "0505050505", category: "japanese")
+  Restaurant.create!(name: "Tapapié", address: "2 avenue de je sais pas", phone_number: "0505050505", category: "chinese")
+  Restaurant.create!(name: "CHéPO", address: "2 avenue de je sais pas", phone_number: "0505050505", category: "italian")
+  Restaurant.create!(name: "Trololo", address: "2 avenue de je sais pas", phone_number: "0505050505", category: "french")
+  Restaurant.create!(name: "Wesh kebab", address: "2 avenue de je sais pas", phone_number: "0505050505", category: "belgian")
+end
